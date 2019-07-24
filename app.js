ba8d41b400196cf38119e020d342a9313f5ec100
Kevin Hsieh <bqh5026@Kevins-iMac.local>
2019-07-23 23:43:53 -0400

    Initial deployment

---
+++ app.js
@@ -0,0 +1,38 @@
+//jshint esversion:6
+
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    ejs = require('ejs'),
+    app = express();
+
+const items = ['Buy Food', 'Cook Food', 'Eat Food'];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get('/', (req, res)=> {
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render('list', {kindOfDay: day, newListItems: items});
+});
+
+app.post('/', (req, res)=> {
+  let item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
+});
+
+app.listen(3000, ()=> {
+    console.log('server is up and running');
+  });
