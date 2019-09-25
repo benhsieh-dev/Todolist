3b2113097a498d3e553580324f422f3ad85fff4f
Ben Hsieh <bqh5026@Kevins-iMac.local>
2019-09-25 00:44:22 -0400

    Add /work route as separate list from regular to do list; create separate footer and header ejs files

--- Readme.md
+++ Readme.md
@@ -3,5 +3,6 @@
 This to do list app allows you to keep track of to do things for the day
 * Click '+' to add to do list item(s)
 * Click checkbox to cross out Item
+* Include /work route as separate list from regular to do list
 
 Corresponding Heroku site Heroku site@ https://still-thicket-12557.herokuapp.com/
--- app.js
+++ app.js
@@ -5,15 +5,18 @@ const express = require('express'),
     ejs = require('ejs'),
     app = express();
 
-const items = ['Buy Food', 'Cook Food', 'Eat Food'];
+let items = ['Buy Food', 'Cook Food', 'Eat Food'];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
+
   let today = new Date();
+
   let options = {
     weekday: "long",
     day: "numeric",
@@ -22,15 +25,29 @@ app.get('/', (req, res)=> {
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render('list', {kindOfDay: day, newListItems: items});
+  res.render('list', {listTitle: day, newListItems: items});
 });
 
-app.post('/', (req, res)=> {
+app.post('/', function(req, res){
   let item = req.body.newItem;
 
-  items.push(item);
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
 
-  res.redirect("/");
+app.get("/work", function(req, res){
+  res.render("List", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 const port = process.env.PORT || 3000;
